d3f8b8d6a5afc8f161079f2a35bedcafa535c3a2
Guilherme Meireles <Guimeirelesgg@gmail.com>
2021-01-27 14:50:01 -0300

    Inserindo o aquivo Phyton

---
+++ Exemplo.py
@@ -0,0 +1,4 @@
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+endereco = input("Informe o seu endereço: ")
+print("Olá ",nome," você tem ", idade," anos e seu endereço é ",endereco)
