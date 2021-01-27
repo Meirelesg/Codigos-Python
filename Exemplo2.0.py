904a32cf19e2087a00c59153527a7e0e83087049
Guilherme Meireles <Guimeirelesgg@gmail.com>
2021-01-27 15:45:21 -0300

    Arquivo Exemplo 2 Python

---
+++ Exemplo2.0.py
@@ -0,0 +1,14 @@
+# Operadores em Pythom é: + - * / %
+# Extra: Operador de expoente **  Exemplo ---> 2**5 = 32
+'''
+Comentário de várias linhas 
+UUUAAAAAAAAAAAAAAUUUUUUUUUUUUU
+num1 = 10
+num2 = 5
+print(num1 * num2)
+'''
+
+nota1 = float (input("Diz a tua nota um aí Junin: "))
+nota2 = float (input("Diz a tua nota dois aí Junin: "))
+media = (nota1 + nota2)/2
+print("Sua média é entre as notas {} e {} é {}" .format(nota1,nota2,media))
